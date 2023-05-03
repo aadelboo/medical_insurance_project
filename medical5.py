e2bd240c48a69a1930a702dfbbaf42d83d193b8e
aadel bouzambou <abouzambou@gmail.com>
2023-05-03 20:40:25 +0200

    working with Python lists on medical insurance costs project

--- medical5.py
+++ medical5.py
@@ -8,6 +8,49 @@ insurance_costs = [
 ]
 
 # Add your code here
+
+# Append a new individual, "Priscilla", to names and 8320.0 to insurance_costs.
 names.append("Priscilla")
 insurance_costs.append(8320.0)
-medical_records = 
+
+# Create a new variable called medical_records that combines insurance_costs and names into a list using the zip() function.
+medical_records = list(zip(insurance_costs, names))
+print(medical_records)
+
+# Print out medical_records in order to see the list.
+num_medical_records = len(medical_records)
+print("There are " + str(num_medical_records) + " medical records")
+
+# Select the first medical record in medical_records, and save it to a variable called first_medical_record.
+first_medical_record = medical_records[0]
+print("Here is the first medical record: " + str(first_medical_record))
+
+# Sort the medical records alphabetically by name
+medical_records.sort()
+print("Here are the medical records sorted by insurance cost: " +
+      str(medical_records))
+
+# The three cheapest insurance costs in our medical records
+cheapest_three = medical_records[:3]
+print("Here are the three cheapest insurance costs in our medical records : " +
+      str(cheapest_three))
+
+# The three most expensive insurance costs in our medical records
+priciest_three = medical_records[-3:]
+print(
+    "Here are the three most expensive insurance costs in our medical records: "
+    + str(priciest_three))
+
+# Count the number of occurrences of "Paul" in our medical records list, and store the result in a variable called occurrences_paul.
+occurrences_paul = names.count("Paul")
+print("There are " + str(occurrences_paul) +
+      " individuals with the name Paul in our medical records.")
+
+# Sort the medical records alphabetically by name
+medical_records_alpha = sorted(list(zip(names, insurance_costs)))
+print("This time the list is alphabetically sorted: " +
+      str(medical_records_alpha))
+
+# The middle five records
+middle_five_records = medical_records[3:8]
+print(middle_five_records)
